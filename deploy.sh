c4119c398db19ad33391e6f324c116c000bde352
Cagri Sofuoglu <cagri.sofuoglu@gmail.com>
2016-05-30 19:54:21 +0300

    fix: changed latex make deploy func name.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Source with zsh/bash
 
 # Setup latex makefile
-makelatex(){
+latexmake(){
     target=$1
     curl -O https://raw.githubusercontent.com/cagri2292/pdflatex-makefile/master/Makefile.include
     curl -O https://raw.githubusercontent.com/cagri2292/pdflatex-makefile/master/makefile
